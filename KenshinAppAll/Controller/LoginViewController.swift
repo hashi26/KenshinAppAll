0f72ddbfdad5e5b3eb05b70994ae9c7d403996a2
12harada <12haradaut@gmail.com>
2019-02-08 14:05:40 +0900

    落ちないバージョン

--- KenshinAppAll/Controller/LoginViewController.swift
+++ KenshinAppAll/Controller/LoginViewController.swift
@@ -17,7 +17,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         //下記は原田CoreDataテストのため記述
-        /*
+        
         let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
         let context:NSManagedObjectContext = appDelegate.managedObjectContext
         let entity = NSEntityDescription.entity(forEntityName: "Goh", in: context)
@@ -33,15 +33,16 @@ class LoginViewController: UIViewController {
         }catch{
             print(error)
         }
- */
         
         //Goh情報をJsonファイルから読み出す
+        /*
         guard let data1 = try? getJSONData1() else { return }
         print(data1)
         goh = try! JSONDecoder().decode([GohClass].self, from: data1!)
         
         print("読み込んだgohデータの値")
         print(goh.count)
+        */
         
         // Do any additional setup after loading the view.
     }
