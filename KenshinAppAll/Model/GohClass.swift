3e6234cede2e6192dd6d119f5a8aea2fb21c1394
12harada <12haradaut@gmail.com>
2019-02-10 05:44:30 +0900

    GohClassからGohへの型変換メソッド追加

--- KenshinAppAll/Model/GohClass.swift
+++ KenshinAppAll/Model/GohClass.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import CoreData //NSManagedObjectContext利用のため
+import UIKit  //UIApplication利用のため
 
 class GohClass: Codable {
     //所在地行政区画CD
@@ -36,19 +38,56 @@ class GohClass: Codable {
     }
 }
 
-func readGohClass() -> [GohClass]{
-    var goh2: [GohClass] = []
-    guard let data1 = try? getJSONData1() else { return goh2}
+//GohClass用のJCLを読み込むメソッド
+func readGohClassJson() -> [GohClass]{
+    var goh: [GohClass] = []
+    guard let data1 = try? getJSONData1() else { return goh}
     //print(data1)
     let decoder = JSONDecoder()
     decoder.dateDecodingStrategy = .iso8601
-    goh2 = try! decoder.decode([GohClass].self, from: data1!)
-    return goh2
+    goh = try! decoder.decode([GohClass].self, from: data1!)
+    return goh
 }
 
+func sampleFunc() {
+    print("sample")
+}
+
+//GohClassテーブルからデータを全件取得するメソッド
+func readGohClass() -> [Goh]{
+    
+    let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    let context:NSManagedObjectContext = appDelegate.managedObjectContext
+    
+    var goh: [Goh] = []
+    let fetchRequest:NSFetchRequest<Goh> = Goh.fetchRequest()
+    let fetchData = try! context.fetch(fetchRequest)
+    if(!fetchData.isEmpty){
+        for i in 0..<fetchData.count{
+            goh.append(fetchData[i])
+        }
+    }
+   return goh
+}
+
+//GohClassのjsonを読み込むための処理
+//forResource:jsonのファイル名
 func getJSONData1() throws -> Data? {
     guard let path = Bundle.main.path(forResource: "goh", ofType: "json") else { return nil }
     let url = URL(fileURLWithPath: path)
     
     return try Data(contentsOf: url)
+}
+
+//GohClassからGohへ変換するためのメソッド
+func gohClassToGoh(gohClass:GohClass) -> Goh{
+    let goh = Goh()
+    goh.created_at = Date() as NSDate
+    goh.gou_ban = gohClass.gou_ban
+    goh.locations_code = gohClass.locations_code
+    goh.towns_name_c = gohClass.towns_name_c
+    goh.towns_name_j = gohClass.towns_name_j
+    goh.updated_at = Date() as NSDate
+    
+    return goh
 }
