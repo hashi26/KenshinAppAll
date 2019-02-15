397679f79e22f6098afa7d155774f11d39518baa
Junpeipei <tgi08084488525@icloud.com>
2019-02-15 14:17:54 +0900

    JsonInsert作成

--- KenshinAppAll/Model/ReadingResultClass.swift
+++ KenshinAppAll/Model/ReadingResultClass.swift
@@ -10,65 +10,14 @@ import CoreData
 import UIKit
 import Foundation
 
-class ReadingResultClass: Codable {
-    //ガスメータ設置場所番号
-    let gmt_set_no : String
-    //検針年月日
-    let knsn_ymd : String
-    //メーター指示数
-    let gmt_sizi_su : Int16
-    //開閉栓状態
-    let is_opend : String
-    //ガス使用量
-    let gas_usage : Int16
-    //検針担当者番号
-    let knsn_tnt_emp_no : String
-    //契約開始年月日
-    let constract_started_at : String
-    //検針方法コード
-    let knsn_method : String
-    //当日検針日時
-    let readed_at : Date
-    //登録日時
-    let created_at : Date
-    //更新日時
-    let updated_at : Date
-    
-    init(gmt_set_no : String ,
-         knsn_ymd : String ,
-         gmt_sizi_su : Int16 ,
-         is_opend : String ,
-         gas_usage : Int16 ,
-         knsn_tnt_emp_no : String ,
-         constract_started_at : String ,
-         knsn_method : String ,
-         readed_at : Date ,
-         created_at : Date ,
-         updated_at : Date
-        ){
-        self.gmt_set_no = gmt_set_no
-        self.knsn_ymd = knsn_ymd
-        self.gmt_sizi_su = gmt_sizi_su
-        self.is_opend = is_opend
-        self.gas_usage = gas_usage
-        self.knsn_tnt_emp_no = knsn_tnt_emp_no
-        self.constract_started_at = constract_started_at
-        self.knsn_method = knsn_method
-        self.readed_at = readed_at
-        self.created_at = created_at
-        self.updated_at = updated_at
-    }
-}
-
-
 //ReadingResultClass用のJCLを読み込むメソッド
-func readReadingResultClassJson() -> [ReadingResultClass]{
-    var readingResult: [ReadingResultClass] = []
+func readReadingResultClassJson() -> [ReadingResultJsonInsert]{
+    var readingResult: [ReadingResultJsonInsert] = []
     guard let data1 = try? getJSONData1() else { return readingResult}
     //print(data1)
     let decoder = JSONDecoder()
     decoder.dateDecodingStrategy = .iso8601
-    readingResult = try! decoder.decode([ReadingResultClass].self, from: data1!)
+    readingResult = try! decoder.decode([ReadingResultJsonInsert].self, from: data1!)
     return readingResult
 }
 
@@ -99,7 +48,7 @@ func getReadingResultJSONData1() throws -> Data? {
 }
 
 //ReadingResultClassからGohへ変換するためのメソッド
-func readingResultClassToReadingResult(readingResultClass:ReadingResultClass) -> Reading_results{
+func readingResultClassToReadingResult(readingResultClass:ReadingResultJsonInsert) -> Reading_results{
     let readingResult = Reading_results()
     readingResult.gmt_set_no = readingResultClass.gmt_set_no
     readingResult.knsn_ymd = readingResultClass.knsn_ymd
