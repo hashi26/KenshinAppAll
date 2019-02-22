6219d83e29c21dfca12c812abf4dc560fbe36e9e
maiooo2 <navy.blue.02mi@gmail.com>
2019-02-22 16:48:14 +0900

    お客さま一覧画面とお客さま画面を分けるため、Customerストーリーボード にView追加。 また、上記に伴いCustomerListViewController追加。

---
+++ KenshinAppAll/Controller/CustomerListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomerListViewController.swift
+//  KenshinAppAll
+//
+//  Created by MaiInagaki on 2019/02/22.
+//  Copyright © 2019年 KenshinT. All rights reserved.
+//
+
+import Foundation
