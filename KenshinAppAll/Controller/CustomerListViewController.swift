233a2c4579db16521843ba433dad773bab1b556f
maiooo2 <navy.blue.02mi@gmail.com>
2019-02-22 20:03:00 +0900

    お客さま画面NavigationVar追加

--- KenshinAppAll/Controller/CustomerListViewController.swift
+++ KenshinAppAll/Controller/CustomerListViewController.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2019年 KenshinT. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+
+class CustomerListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
