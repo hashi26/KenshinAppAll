9e4f96a9f4ce8cc229cadbcebd6a37af0d051b8b
12harada <12haradaut@gmail.com>
2019-03-06 08:57:05 +0900

    CustomerListViewController作成

---
+++ KenshinAppAll/Controller/Config/CustomerListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomerListViewController.swift
+//  KenshinAppAll
+//
+//  Created by Harada Hiroaki on 2019/03/06.
+//  Copyright © 2019 KenshinT. All rights reserved.
+//
+
+import Foundation
