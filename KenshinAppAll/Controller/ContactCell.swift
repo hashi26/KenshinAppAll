cbd9abd94147deab8593a5c2b871c1264872c83c
12harada <12haradaut@gmail.com>
2019-03-07 20:33:30 +0900

    コンタクト画面のカスタムクラス実装

---
+++ KenshinAppAll/Controller/ContactCell.swift
@@ -0,0 +1,26 @@
+//
+//  ContactCell.swift
+//  KenshinAppAll
+//
+//  Created by Harada Hiroaki on 2019/03/07.
+//  Copyright © 2019 KenshinT. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var base: UILabel!
+    
+    func setupCell(model:Reading_person ) {
+        print("setupCell関数の呼び出し")
+        //名前
+        name.text = model.knsn_tnt_emp_no
+        
+        //拠点
+        base.text = model.knsn_tnt_emp_no
+        
+    }
+
+    
+}
