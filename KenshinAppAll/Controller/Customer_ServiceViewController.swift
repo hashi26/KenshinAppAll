6a0c2153113c786c6d17cb092e16ddae92872800
maiooo2 <navy.blue.02mi@gmail.com>
2019-03-08 10:30:29 +0900

    サービステーブルにセクションヘッダ追加

--- KenshinAppAll/Controller/Customer_ServiceViewController.swift
+++ KenshinAppAll/Controller/Customer_ServiceViewController.swift
@@ -12,7 +12,7 @@ class Customer_ServiceViewController: UIViewController, UITableViewDelegate, UIT
     
     var service:Customers = Customers()
     var serviceItem:[String] = []
-    let sectionName:[String] = ["社番","建物名"]
+    let sectionName:[String] = ["名前（カナ）","社番","建物名","メモ"]
     //Cell上部に上記のタイトルを表示したいが表示できない
     // 参照：https://tech.pjin.jp/blog/2016/09/06/tableview-12/
     
@@ -24,32 +24,40 @@ class Customer_ServiceViewController: UIViewController, UITableViewDelegate, UIT
         super.didReceiveMemoryWarning()
     }
     
-    //セルの個数を指定するデリゲートメソッド
+    //各セクション内のセル数を設定するデリゲートメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         service = appDelegate.customerInfo
-        //print(service[0].gmt_set_no!)
-        
         insertItem()
-        return serviceItem.count
-        
-        //return 1
+        return 1
     }
     
     //セルに値を設定するデータソースメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルを取得する
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         // セルに表示する値を設定する
-        cell.textLabel?.text = serviceItem[indexPath.row]
+        cell.textLabel?.text = serviceItem[indexPath.section]
         return cell
+    }
+    
+    // セクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    // セクションヘッダ
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?  {
+        return sectionName[section]
     }
     
     // サービスに表示する項目の件数
     func insertItem(){
         var item: [String] = []
+        item.append(String(service.customer_name_cana!))
         item.append(String(service.meter_no!))
         item.append(String(service.tatemono_kanzi!))
+        item.append(String(service.memo!))
         serviceItem = item
     }
 }
