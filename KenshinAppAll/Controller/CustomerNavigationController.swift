796f07d6e4f54def654db997f7122dd104699ad7
maiooo2 <navy.blue.02mi@gmail.com>
2019-03-18 16:51:22 +0900

    MapからCustomerへの値渡しを実装

--- KenshinAppAll/Controller/CustomerNavigationController.swift
+++ KenshinAppAll/Controller/CustomerNavigationController.swift
@@ -10,19 +10,7 @@ import UIKit
 
 class CustomerNavigationController: UINavigationController{
     
-    var customers:[Customers] = []
-    var selectionNumber: Int = 0
+    var customers:[Customers]?
+    var selectionNumber: Int?
     
-   
-    
-    //遷移先の画面を取り出す
-    override func prepare(for segue:UIStoryboardSegue, sender: Any?){
-        print("次画面呼び出し実行 MAP→お客さま")
-        //次の画面を取り出す
-        let viewController = segue.destination as! CustomerViewController
-        viewController.customers = self.customers
-        viewController.selectionNumber = self.selectionNumber
-        print("選択配列番号：")
-        print(selectionNumber)
-    }
 }
